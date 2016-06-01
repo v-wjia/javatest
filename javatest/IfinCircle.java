7a2668280247e45c5d91821f38f4e55fa2b8b4ce
Jia Wang (Wicresoft) <v-wjia@microsoft.com>
2016-06-01 16:39:46 +0800

    add IfinCircle.java

---
+++ javatest/IfinCircle.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatest;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author v-wjia
+ */
+public class IfinCircle {
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a point with two cordinates:");
+        double x = input.nextInt();
+        double y = input.nextInt();
+        double CircleRadius = 10.0;
+        double distance = Math.sqrt(Math.pow(x, 2)+ Math.pow(y, 2));
+        if (distance >= 10) {
+            System.out.println("Points are outside the circle." + distance);
+        } else {
+            System.out.println("Points are inside the circle." + distance);
+        }
+    }
+}
